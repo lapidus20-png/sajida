d101fbd47980c5baa7a88e780cffbbf4fcecc403
lapidus20-png <lapidus20@googlemail.com>
2025-12-07 06:18:13 +0000

    Updated FIX_DATABASE_NOW.sql

--- FIX_DATABASE_NOW.sql
+++ FIX_DATABASE_NOW.sql
@@ -1,28 +1,13 @@
 -- RUN THIS IN SUPABASE SQL EDITOR
--- This will fix all RLS issues and allow signup to work
+-- This will fix RLS issues on tables we control
 
--- 1. Disable RLS on auth.users
-ALTER TABLE IF EXISTS auth.users DISABLE ROW LEVEL SECURITY;
-
--- 2. Drop all policies on auth.users
-DO $$
-DECLARE pol record;
-BEGIN
-    FOR pol IN
-        SELECT policyname FROM pg_policies
-        WHERE schemaname = 'auth' AND tablename = 'users'
-    LOOP
-        EXECUTE format('DROP POLICY IF EXISTS %I ON auth.users', pol.policyname);
-    END LOOP;
-END $$;
-
--- 3. Remove problematic triggers
+-- 1. Remove problematic triggers (if any exist)
 DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users CASCADE;
 DROP TRIGGER IF EXISTS on_auth_user_created_v2 ON auth.users CASCADE;
 DROP TRIGGER IF EXISTS ensure_user_profile_trigger ON auth.users CASCADE;
 DROP TRIGGER IF EXISTS create_profile_on_signup ON auth.users CASCADE;
 
--- 4. Disable RLS on public tables
+-- 2. Disable RLS on public tables (temporary fix)
 ALTER TABLE IF EXISTS public.users DISABLE ROW LEVEL SECURITY;
 ALTER TABLE IF EXISTS public.artisans DISABLE ROW LEVEL SECURITY;
 ALTER TABLE IF EXISTS public.jobs DISABLE ROW LEVEL SECURITY;
@@ -33,7 +18,7 @@ ALTER TABLE IF EXISTS public.notifications DISABLE ROW LEVEL SECURITY;
 ALTER TABLE IF EXISTS public.payments DISABLE ROW LEVEL SECURITY;
 ALTER TABLE IF EXISTS public.contact_requests DISABLE ROW LEVEL SECURITY;
 
--- 5. Drop all policies on public tables
+-- 3. Drop all existing policies on public tables
 DO $$
 DECLARE
   pol record;
@@ -51,9 +36,9 @@ BEGIN
   END LOOP;
 END $$;
 
--- 6. Drop problematic functions
-DROP FUNCTION IF EXISTS create_user_profile CASCADE;
-DROP FUNCTION IF EXISTS handle_new_user CASCADE;
-DROP FUNCTION IF EXISTS ensure_user_profile CASCADE;
-DROP FUNCTION IF EXISTS is_admin CASCADE;
-DROP FUNCTION IF EXISTS has_artisan_profile CASCADE;
+-- 4. Drop problematic functions
+DROP FUNCTION IF EXISTS public.create_user_profile CASCADE;
+DROP FUNCTION IF EXISTS public.handle_new_user CASCADE;
+DROP FUNCTION IF EXISTS public.ensure_user_profile CASCADE;
+DROP FUNCTION IF EXISTS public.is_admin CASCADE;
+DROP FUNCTION IF EXISTS public.has_artisan_profile CASCADE;
